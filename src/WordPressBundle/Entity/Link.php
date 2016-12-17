03a4e79ab7625aa663462d898bbb810009434458
Thomas DENEULIN <thomasdeneulin@gmail.com>
2016-12-17 16:42:23 +0100

    :wrench: Generate entities for Link

--- src/WordPressBundle/Entity/Link.php
+++ src/WordPressBundle/Entity/Link.php
@@ -57,4 +57,134 @@ class Link
     protected $linkDescription;
 
 
+
+    /**
+     * Get linkId
+     *
+     * @return integer
+     */
+    public function getLinkId()
+    {
+        return $this->linkId;
+    }
+
+    /**
+     * Set linkUrl
+     *
+     * @param string $linkUrl
+     *
+     * @return Link
+     */
+    public function setLinkUrl($linkUrl)
+    {
+        $this->linkUrl = $linkUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get linkUrl
+     *
+     * @return string
+     */
+    public function getLinkUrl()
+    {
+        return $this->linkUrl;
+    }
+
+    /**
+     * Set linkName
+     *
+     * @param string $linkName
+     *
+     * @return Link
+     */
+    public function setLinkName($linkName)
+    {
+        $this->linkName = $linkName;
+
+        return $this;
+    }
+
+    /**
+     * Get linkName
+     *
+     * @return string
+     */
+    public function getLinkName()
+    {
+        return $this->linkName;
+    }
+
+    /**
+     * Set linkImage
+     *
+     * @param string $linkImage
+     *
+     * @return Link
+     */
+    public function setLinkImage($linkImage)
+    {
+        $this->linkImage = $linkImage;
+
+        return $this;
+    }
+
+    /**
+     * Get linkImage
+     *
+     * @return string
+     */
+    public function getLinkImage()
+    {
+        return $this->linkImage;
+    }
+
+    /**
+     * Set linkTarget
+     *
+     * @param string $linkTarget
+     *
+     * @return Link
+     */
+    public function setLinkTarget($linkTarget)
+    {
+        $this->linkTarget = $linkTarget;
+
+        return $this;
+    }
+
+    /**
+     * Get linkTarget
+     *
+     * @return string
+     */
+    public function getLinkTarget()
+    {
+        return $this->linkTarget;
+    }
+
+    /**
+     * Set linkDescription
+     *
+     * @param string $linkDescription
+     *
+     * @return Link
+     */
+    public function setLinkDescription($linkDescription)
+    {
+        $this->linkDescription = $linkDescription;
+
+        return $this;
+    }
+
+    /**
+     * Get linkDescription
+     *
+     * @return string
+     */
+    public function getLinkDescription()
+    {
+        return $this->linkDescription;
+    }
 }
