adfa6d74ece21e008c108b93e65538430563dab2
Thomas DENEULIN <thomasdeneulin@gmail.com>
2016-12-28 15:34:53 +0100

    :sparkles: Add login decorator for tests on behat

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,6 +4,8 @@ use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Defines application features from the specific context.
@@ -37,9 +39,10 @@ class FeatureContext implements Context, SnippetAcceptingContext
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
-    public function __construct(ManagerRegistry $doctrine)
+    public function __construct(ManagerRegistry $doctrine, KernelInterface $kernel)
     {
         $this->doctrine   = $doctrine;
+        $this->kernel     = $kernel; 
         $this->manager    = $doctrine->getManager();
         $this->schemaTool = new SchemaTool($this->manager);
         $this->classes    = $this->manager->getMetadataFactory()->getAllMetadata();
@@ -59,5 +62,34 @@ class FeatureContext implements Context, SnippetAcceptingContext
     public function dropDatabase()
     {
         $this->schemaTool->dropSchema($this->classes);
+    }
+
+    /**
+     * @BeforeScenario @Login
+     */
+    public function login($event) 
+    {
+
+        $response = $this->kernel->handle(Request::create(
+            '/oauth/token',
+            'POST',
+            [],
+            [],
+            [],
+            array(
+                'CONTENT_TYPE' => 'application/json'
+            ),
+            array(
+                "client_id"     => "client_id_1",
+                "client_secret" => "client_secret_1",
+                "grant_type"    => "password",
+                "username"      => "admin",
+                "password"      => "admin",
+                "scope"         => "user"
+            )
+        ));
+        $data = json_decode($response->getContent());
+        $event->getEnvironment()->getContext("Sanpi\Behatch\Context\RestContext")->iAddHeaderEqualTo("Authorization", "Bearer " . $data->access_token);
+
     }
 }
