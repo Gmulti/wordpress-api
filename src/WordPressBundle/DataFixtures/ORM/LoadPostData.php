5d048b30d4fbf3f2c60bd488ac280da878cd8ea3
Thomas DENEULIN <thomasdeneulin@gmail.com>
2017-01-04 22:45:57 +0100

    :card_index: Add post fixtures

--- src/WordPressBundle/DataFixtures/ORM/LoadPostData.php
+++ src/WordPressBundle/DataFixtures/ORM/LoadPostData.php
@@ -23,10 +23,23 @@ class LoadPostData extends AbstractFixture implements OrderedFixtureInterface
         $post1->setPostExcerpt("lorem ipsum dolor sit");
         $post1->setPostModified(new \DateTime("now"));
         $post1->setPostModifiedGmt(new \DateTime("now"));
+  
+        $post2 = new Post();
+        $post2->setPostAuthor(1);
+        $post2->setPostDate(new \DateTime("now"));
+        $post2->setPostDateGmt(new \DateTime("now"));
+        $post2->setPostContent("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam eligendi provident quam eveniet sint nesciunt consequuntur reprehenderit velit est molestiae, quisquam, repellendus, aliquid delectus natus! Et optio asperiores, commodi dignissimos. <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam rem nemo animi consequatur optio aut unde numquam, sed cumque ullam labore explicabo necessitatibus mollitia consectetur. Et quasi hic inventore ratione.</div>
+        <div>Amet recusandae maiores dolores sequi voluptate quas, quis incidunt molestias commodi qui accusamus labore ipsum, quibusdam culpa voluptatem sapiente. Non voluptatem eum commodi ipsa labore eveniet! Iusto voluptates sint, dolores.</div>");
+        $post2->setPostTitle("Lorem ipsum 2");
+        $post2->setPostExcerpt("lorem ipsum dolor sit 2");
+        $post2->setPostModified(new \DateTime("now"));
+        $post2->setPostModifiedGmt(new \DateTime("now"));
 
         $this->addReference('post-1', $post1);
+        $this->addReference('post-2', $post2);
     
         $manager->persist($post1);
+        $manager->persist($post2);
         $manager->flush();
 
     }
